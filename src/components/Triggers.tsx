05174643baae33f4981fd838357a49a1c4fc4b09
msanje <m.sanjayachar@gmail.com>
2025-09-16 12:16:23 +0530

    drawer checkpoint

--- src/components/Triggers.tsx
+++ src/components/Triggers.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { triggerOptions } from "../data/triggerOptions";
 
 function Triggers({
@@ -9,19 +10,29 @@ function Triggers({
   trigger?: any;
   close: () => void;
 }) {
+  const [selectedTrigger, setSelectedTrigger] = useState<string | null>(null);
+
   return (
     <div
-      className={`absolute top-0 right-0 h-full w-80 bg-white dark:bg-gray-800 shadow-lg transform transition-transform duration-200 ${open ? "translate-x-0" : "translate-x-full"}`}
+      className={`absolute top-0 px-4 py-4 right-0 h-full w-[450px] bg-white dark:bg-gray-400 shadow-lg transform transition-transform duration-200 ${open ? "translate-x-0" : "translate-x-full"}`}
     >
-      <h1>What Triggers this workflow?</h1>
-      <p>A trigger is a step that starts your workflow</p>
-      <input placeholder="Search nodes..." />
+      <h1 className="text-xl">What Triggers this workflow?</h1>
+      <p className="text-gray-300">
+        A trigger is a step that starts your workflow
+      </p>
+      <input className="w-full" placeholder="Search nodes..." />
 
       {triggerOptions.map((option, idx) => (
-        <div key={idx}>
-          <h1>{option.title}</h1>
-          <p>{option.description}</p>
+        <div
+          key={idx}
+          className={`-mx-4 px-8 w-[450px] h-24 flex items-center gap-4 cursor-pointer ${selectedTrigger === option.id ? "border-l-4 border-l-orange-500" : "hover:border-l-4 hover:border-l-white"}`}
+          onClick={() => setSelectedTrigger(option.id)}
+        >
           <option.icon />
+          <div>
+            <h1>{option.title}</h1>
+            <p className="text-sm text-gray-500">{option.description}</p>
+          </div>
         </div>
       ))}
 
